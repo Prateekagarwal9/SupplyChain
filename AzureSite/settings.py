839e8d413a2cb28345bd913131caea4c74fe47af
Prateekagarwal9 <39221904+Prateekagarwal9@users.noreply.github.com>
2019-09-04 19:55:33 +0530

    Update settings.py

--- AzureSite/settings.py
+++ AzureSite/settings.py
@@ -14,7 +14,9 @@ import os
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-
+TEMPLATE_DIR = os.path.join(BASE_DIR, 'templates')
+STATIC_DIR = os.path.join(BASE_DIR, 'SupplyChain/static')
+MEDIA_DIR = os.path.join(BASE_DIR, 'media')
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/2.0/howto/deployment/checklist/
@@ -62,14 +64,13 @@ MIDDLEWARE = [
 ]
 
 ROOT_URLCONF = 'AzureSite.urls'
-STATIC_URL = '/home/site/wwwroot/SupplyChain/static/'
+STATIC_URL = '/SupplyChain/static/'
 
 
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [os.path.join(BASE_DIR, 'templates'), os.path.join(BASE_DIR, 'SupplyChain/static')]
-        ,
+        'DIRS': [],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
